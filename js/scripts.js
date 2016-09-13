471d41377a4b5a34e749e4e91078b22ac96ec3b8
dan <bobalo16@gmail.com>
2016-09-13 11:02:41 -0400

    Changed example image to YMCA

--- js/scripts.js
+++ js/scripts.js
@@ -56,39 +56,39 @@ var redIcon = new RedIcon();
 //--BEGIN MARKERS--
 //Add marker to Dining Hall
 L.marker([-12, 18.9375], {icon: redIcon}).addTo(map)
-    .bindPopup('Customizable popup! <br><br> <img src="https://www.google.ca/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" style="max-width: 120px;" />')
+    .bindPopup('Customizable popup! <br><br> <img src="https://gv.ymca.ca/CWP/media/YGV/ymcagv.png" style="max-width: 100%;" />')
 
 //Add marker to Upper Archery
 L.marker([-9.6875, 8.49375], {icon: redIcon}).addTo(map)
-    .bindPopup('Customizable popup! <br><br> <img src="https://www.google.ca/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" style="max-width: 120px;" />')
+    .bindPopup('Customizable popup! <br><br> <img src="https://gv.ymca.ca/CWP/media/YGV/ymcagv.png" style="max-width: 100%;" />')
 
 //Add marker to Lower Archery
 L.marker([-19.1875, 10.15625], {icon: redIcon}).addTo(map)
-    .bindPopup('Customizable popup! <br><br> <img src="https://www.google.ca/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" style="max-width: 120px;" />')
+    .bindPopup('Customizable popup! <br><br> <img src="https://gv.ymca.ca/CWP/media/YGV/ymcagv.png" style="max-width: 100%;" />')
 
 //Add marker to Cabins
 L.marker([-7.875, 28.53125], {icon: redIcon}).addTo(map)
-    .bindPopup('Customizable popup! <br><br> <img src="https://www.google.ca/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" style="max-width: 120px;" />')
+    .bindPopup('Customizable popup! <br><br> <img src="https://gv.ymca.ca/CWP/media/YGV/ymcagv.png" style="max-width: 100%;" />')
 
 //Add marker to Wash Hut
 L.marker([-5.4375, 22.96875], {icon: redIcon}).addTo(map)
-    .bindPopup('Customizable popup! <br><br> <img src="https://www.google.ca/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" style="max-width: 120px;" />')
+    .bindPopup('Customizable popup! <br><br> <img src="https://gv.ymca.ca/CWP/media/YGV/ymcagv.png" style="max-width: 100%;" />')
 
 //Add marker to Rec Hall
 L.marker([-19.3125, 29.78125], {icon: redIcon}).addTo(map)
-    .bindPopup('Customizable popup! <br><br> <img src="https://www.google.ca/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" style="max-width: 120px;" />')
+    .bindPopup('Customizable popup! <br><br> <img src="https://gv.ymca.ca/CWP/media/YGV/ymcagv.png" style="max-width: 100%;" />')
 
 //Add marker to Boat House
 L.marker([-23.375, 10.125], {icon: redIcon}).addTo(map)
-    .bindPopup('Customizable popup! <br><br> <img src="https://www.google.ca/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" style="max-width: 120px;" />')
+    .bindPopup('Customizable popup! <br><br> <img src="https://gv.ymca.ca/CWP/media/YGV/ymcagv.png" style="max-width: 100%;" />')
 
 //Add marker to E-Lodge
 L.marker([-22.09375, 4.34375], {icon: redIcon}).addTo(map)
-    .bindPopup('Customizable popup! <br><br> <img src="https://www.google.ca/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" style="max-width: 120px;" />')
+    .bindPopup('Customizable popup! <br><br> <img src="https://gv.ymca.ca/CWP/media/YGV/ymcagv.png" style="max-width: 100%;" />')
 
 //Add marker to Ad Hall
 L.marker([-18.21875, 20.8125], {icon: redIcon}).addTo(map)
-    .bindPopup('Customizable popup! <br><br> <img src="https://www.google.ca/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" style="max-width: 120px;" />')
+    .bindPopup('Customizable popup! <br><br> <img src="https://gv.ymca.ca/CWP/media/YGV/ymcagv.png" style="max-width: 100%;" />')
 //--END MARKERS--
 
 //Open a popup with the related info when the user clicks the indicator
